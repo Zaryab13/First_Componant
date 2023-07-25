8514bfa9c1d212ab6f75018be689e76c2b7f6cf0
Zaryab Khan <93703454+Zaryab13@users.noreply.github.com>
2023-07-25 01:23:05 -0700

    Added Some More Componants UI based

--- src/App.js
+++ src/App.js
@@ -1,51 +1,31 @@
-import ExpenseItem from "./Componants/ExpenseItem";
+import Expenses from "./Componants/Expenses";
 
 function App() {
   const expenses = [
     {
       title: "Groceries",
       amount: 50.25,
-      date: new Date(2023, 7, 24),
+      date: new Date("2023, 7, 24"),
     },
     {
       title: "Gasoline",
       amount: 30.5,
-      date: new Date(2023, 7, 24),
+      date: new Date("2023, 7, 24"),
     },
     {
       title: "Dinner",
       amount: 75.0,
-      date: new Date(2023, 7, 24),
+      date: new Date("2023, 7, 24"),
     },
     {
       title: "Movie Tickets",
       amount: 40.75,
-      date: new Date(2023, 7, 24),
+      date: new Date("2023, 7, 24"),
     },
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }
