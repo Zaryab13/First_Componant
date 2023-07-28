a9ae273a08d9310e8148740c49f1e680c534b058
Zaryab Khan <93703454+Zaryab13@users.noreply.github.com>
2023-07-28 16:34:05 +0500

    Lifting state Up and Using Make on dynamic data

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
-import React from 'react';
+import React , { useState } from 'react';
 
 import NewExpenses from './Componants/NewExpense/NewExpenses';
 import Expenses from './Componants/Expenses/Expenses';
 
+const DUMMY_EXP = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+
+const [expenses, setExpense] = useState(DUMMY_EXP);
+
   const newExpenseDataHandler = (expense) => {
-    console.log(expense);
+    setExpense((prevState)=>{
+      console.log([...prevState, expense])
+      return [...prevState, expense];
+    });
   }
   
   return (
